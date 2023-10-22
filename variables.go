cc00c4a72f10245c732f89bfc3adf9bb45d3ed42
igsr5 <igsr5.dev@gmail.com>
2023-10-22 23:32:29 +0900

    Update variable values for status and category IDsThe previous variable values for status and category IDs have been updated

--- variables.go
+++ variables.go
@@ -1,27 +1,30 @@
 package variables
 
-// TODO: Implement
 var (
 	// IN_PROGRESS_STATUS_ID is a status id for "In Progress" column.
-	IN_PROGRESS_STATUS_ID = "1234"
+	IN_PROGRESS_STATUS_ID = "47fc9ee4"
 	// IN_PENDING_STATUS_ID is a status id for "In Pending" column.
-	IN_PENDING_STATUS_ID = "5678"
+	IN_PENDING_STATUS_ID = "e0c049ea"
 	// COMPLETE_STATUS_ID is a status id for "Complete" column.
-	COMPLETE_STATUS_ID = "9012"
+	COMPLETE_STATUS_ID = "f2f3bb33"
 )
 
-// TODO: Implement
 var (
 	// PROJECT_ID is a project id.
-	PROJECT_ID = "1234"
+	PROJECT_ID = "227"
 )
 
-// TODO: Implement
 var (
 	// ISSUE_CATEGORY_ID is a category id for issues.
-	ISSUE_CATEGORY_ID = "1234"
+	ISSUE_CATEGORY_ID = "b41c9a4b"
 	// PR_CATEGORY_ID is a category id for review.
-	PR_CATEGORY_ID = "5678"
+	PR_CATEGORY_ID = "086cc5ba"
 	// REVIEW_PR_CATEGORY_ID a status id for "In Progress" column.
-	REVIEW_PR_CATEGORY_ID = "9012"
+	REVIEW_PR_CATEGORY_ID = "5f8f94e2"
 )
+
+// NOTE: this command can investigate the project data.
+// ```
+// $ gh project view 227 --owner wantedly --format json | jq
+// $ gh project field-list 227 --owner wantedly --format json | jq
+// ```
