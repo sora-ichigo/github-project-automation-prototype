77795d3ec4f1c1d69096a3706d8ee242bb4fbe28
igsr5 <igsr5.dev@gmail.com>
2023-10-22 23:19:54 +0900

    Add project items and move them to specified category and status

--- command/projectv2.go
+++ command/projectv2.go
@@ -1,6 +1,8 @@
 package command
 
 import (
+	"errors"
+
 	usecase "github.com/igsr5/github-project-automation/usecase"
 )
 
@@ -14,6 +16,48 @@ func NewProjectV2Setter() usecase.ProjectV2Setter {
 
 // Set sets project items.
 func (s *projectV2SetterImpl) Set(categoryID string, statusID string, projectItems []usecase.ProjectItem) error {
-	// TODO: Implement
+	// 1. Add project items
+	ids := make([]projectID, 0, len(projectItems))
+	for _, item := range projectItems {
+		id, err := addProjectItem(item.URL)
+		if err != nil {
+			return errors.Join(err)
+		}
+		ids = append(ids, id)
+	}
+
+	// 2. Move project item status to specified status
+	for _, id := range ids {
+		err := moveStatusProjectItem(id, statusID)
+		if err != nil {
+			return errors.Join(err)
+		}
+	}
+
+	// 3. Move project item category to specified category
+	for _, id := range ids {
+		err := moveCategoryProjectItem(id, categoryID)
+		if err != nil {
+			return errors.Join(err)
+		}
+	}
+
+	return nil
+}
+
+type projectID string
+
+func addProjectItem(url string) (projectID, error) {
+	// TODO: implement
+	return "", nil
+}
+
+func moveStatusProjectItem(id projectID, statusID string) error {
+	// TODO: implement
+	return nil
+}
+
+func moveCategoryProjectItem(id projectID, categoryID string) error {
+	// TODO: implement
 	return nil
 }
