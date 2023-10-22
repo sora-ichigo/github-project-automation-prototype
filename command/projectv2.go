ce1cefaf2d58c8acdf64415e3893b99a7a36e0d8
igsr5 <igsr5.dev@gmail.com>
2023-10-22 23:41:09 +0900

    Add project items and move their status and category

--- command/projectv2.go
+++ command/projectv2.go
@@ -1,10 +1,17 @@
 package command
 
 import (
+	"encoding/json"
 	"errors"
+	"os/exec"
 
+	variables "github.com/igsr5/github-project-automation"
 	usecase "github.com/igsr5/github-project-automation/usecase"
 )
+
+type Item struct {
+	ID string `json:"id"`
+}
 
 type projectV2SetterImpl struct {
 }
@@ -17,17 +24,17 @@ func NewProjectV2Setter() usecase.ProjectV2Setter {
 // Set sets project items.
 func (s *projectV2SetterImpl) Set(categoryID string, statusID string, projectItems []usecase.ProjectItem) error {
 	// 1. Add project items
-	ids := make([]projectID, 0, len(projectItems))
+	items := make([]Item, 0, len(projectItems))
 	for _, item := range projectItems {
-		id, err := addProjectItem(item.URL)
+		item, err := addProjectItem(item.URL)
 		if err != nil {
 			return errors.Join(err)
 		}
-		ids = append(ids, id)
+		items = append(items, *item)
 	}
 
 	// 2. Move project item status to specified status
-	for _, id := range ids {
+	for _, id := range items {
 		err := moveStatusProjectItem(id, statusID)
 		if err != nil {
 			return errors.Join(err)
@@ -35,7 +42,7 @@ func (s *projectV2SetterImpl) Set(categoryID string, statusID string, projectIte
 	}
 
 	// 3. Move project item category to specified category
-	for _, id := range ids {
+	for _, id := range items {
 		err := moveCategoryProjectItem(id, categoryID)
 		if err != nil {
 			return errors.Join(err)
@@ -45,19 +52,27 @@ func (s *projectV2SetterImpl) Set(categoryID string, statusID string, projectIte
 	return nil
 }
 
-type projectID string
+func addProjectItem(url string) (*Item, error) {
+	cmd := exec.Command("gh", "project", "item-add", variables.PROJECT_ID, "--owner", "wantedly", "--url", url, "--format", "json")
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, errors.Join(err)
+	}
 
-func addProjectItem(url string) (projectID, error) {
-	// TODO: implement
-	return "", nil
+	var item Item
+	if err := json.Unmarshal(output, &item); err != nil {
+		return nil, errors.Join(err)
+	}
+
+	return &item, nil
 }
 
-func moveStatusProjectItem(id projectID, statusID string) error {
+func moveStatusProjectItem(item Item, statusID string) error {
 	// TODO: implement
 	return nil
 }
 
-func moveCategoryProjectItem(id projectID, categoryID string) error {
+func moveCategoryProjectItem(item Item, categoryID string) error {
 	// TODO: implement
 	return nil
 }
