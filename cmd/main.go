a0a650aa4279bbbfb57212c698019a54b832c84c
igsr5 <igsr5.dev@gmail.com>
2023-10-25 02:24:04 +0900

    Refactor error handling in main function

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,13 @@ import (
 )
 
 func main() {
+	err := run()
+	if err != nil {
+		log.Printf("error: %v", err)
+	}
+}
+
+func run() error {
 	projectV2Setter := command.NewProjectV2Setter()
 	issueFetcher := query.NewIssueFetcher()
 	prFetcher := query.NewPrFetcher()
@@ -23,18 +30,15 @@ func main() {
 
 	for _, a := range automations {
 		if err := a.SetInProgress(); err != nil {
-			log.Printf("error occurred: %v", err)
-			os.Exit(1)
+			return fmt.Errorf("automations is failed: %v", err)
 		}
 
 		if err := a.SetInPending(); err != nil {
-			log.Printf("error occurred: %v", err)
-			os.Exit(1)
+			return fmt.Errorf("automations is failed: %v", err)
 		}
 
 		if err := a.SetComplete(); err != nil {
-			log.Printf("error occurred: %v", err)
-			os.Exit(1)
+			return fmt.Errorf("automations is failed: %v", err)
 		}
 	}
 }
